c3444d92d9f257dbae84d37db93fcf8fda30a467
Noah Shinabarger <noah@forallsecure.com>
2023-02-03 12:14:38 -0500

    Merge pull request #1 from capuanob/mayhem
    Mayhem Heroes Integration

---
+++ mayhem/fuzz_gitignore.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import atheris
+import sys
+import fuzz_helpers
+import random
+
+with atheris.instrument_imports(include=["gitignore_parser"]):
+    from gitignore_parser import parse_gitignore
+
+
+def TestOneInput(data):
+    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+    try:
+        with fdp.ConsumeTemporaryFile(".gitignore", all_data=False, as_bytes=False) as name:
+            matches = parse_gitignore(name)
+            for _ in range(fdp.ConsumeIntInRange(0, 20)):
+                matches('/tmp/' + fdp.ConsumeRandomString())
+    except (ValueError, IndexError) as e:
+        if random.random() > 0.999:
+            raise e
+        return -1
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
